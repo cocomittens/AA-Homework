b696b737d628fee6d6e3986c74613153dfc02eda
ioqren <corriestoddard@gmail.com>
2019-01-11 07:37:00 -0800

    Add w1d5 homework

---
+++ w1d5.rb
@@ -0,0 +1,68 @@
+ class Stack
+
+    attr_reader :stack
+
+    def initialize
+      # create ivar to store stack here!
+      @stack = []
+    end
+
+    def push(el)
+      # adds an element to the stack
+      stack << el
+      el
+    end
+
+    def pop
+      # removes one element from the stack
+      stack.pop
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+      stack.last
+    end
+  
+end
+
+class Queue
+    attr_reader :queue
+    def initialize
+      @queue = []
+    end
+
+    def enqueue(el)
+        queue.unshift(el)
+    end
+    
+    def dequeue
+        queue.shift
+    end
+
+    def peek
+        queue.first
+    end
+end
+
+class Map
+    attr_accessor :pmap
+    def initialize
+      @map = []
+    end
+
+    def set(key, value)
+        
+    end
+    
+    def get(key)
+        
+    end
+
+    def delete(key)
+        
+    end
+
+    def show
+
+    end
+end
