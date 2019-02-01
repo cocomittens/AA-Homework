538bbb8c3375d66db548bc682ba258f931e6a461
ioqren <corriestoddard@gmail.com>
2019-01-31 20:32:00 -0800

    Add w4d5 exercises

---
+++ w4d5/app/models/user.rb
@@ -0,0 +1,43 @@
+require 'securerandom'
+
+class User < ApplicationRecord
+    validates :email, presence: true
+    validates :password_digest, presence: true
+    validates :session_token, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def generate_session_token
+        token = SecureRandom::urlsafe_base64
+        while self.class.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+end
