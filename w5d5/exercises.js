a4e48191b62be47a4e9edfe949ceb87719ffff14
ioqren <corriestoddard@gmail.com>
2019-02-08 09:35:03 -0800

    Update w5d5 exercises

--- w5d5/exercises.js
+++ w5d5/exercises.js
@@ -1,3 +1,9 @@
-function setTimeout() {
-    
+window.setTimeout(function () {
+    alert('HAMMERTIME');
+}, 5000);
+
+function hammerTime(time) {
+    window.setTimeout(function () {
+        alert(`${time} is hammertime!`);
+    }, time);
 }
