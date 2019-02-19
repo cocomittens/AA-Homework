b7027e08752e50d03b49ad609bd26b2229624dda
ioqren <corriestoddard@gmail.com>
2019-02-19 07:12:51 -0800

    Update w7d2 exercises

--- w7d2/frontend/todo_redux.jsx
+++ w7d2/frontend/todo_redux.jsx
@@ -7,19 +7,22 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
-
+  let store = configureStore(preloadedState);
+  store = applyMiddlewares(store, [addLoggingToDispatch]);
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
-  store.dispatch = addLoggingToDispatch(store);
 });
 
-const addLoggingToDispatch = store => {
-    const dispatch = store.dispatch
-    return action => {
-      console.log(store.getState());
-      console.log(action);
-      dispatch(action);
-      console.log(store.getState());
-    }
+const addLoggingToDispatch = store => next => action => {
+  console.log(action);
+  console.log(store.getState());
+  let res = next(action);
+  console.log(store.getState());
+  return res;
+}
+
+const applyMiddlewares = (store, middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach(mw => dispatch = mw(store)(dispatch));
+  return Object.assign({}, store, { dispatch });
 }
